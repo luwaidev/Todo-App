b4fd8b083d96f2710fc86532cc2a215e103cf59a
luhyphenwai <luwaiwong@gmail.com>
2021-09-18 14:37:57 -0400

    all basic functionality

--- App.js
+++ App.js
@@ -1,12 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { Icon } from "react-native-elements";
+import TodoItem from "./src/components/TodoItem";
+
+const windowHeight = Dimensions.get("window").height;
 
 export default function App() {
+  var currentTodo = 0;
+  const [todos, setTodos] = useState([[]]);
+  const [text, onChangeText] = React.useState("Useless Text");
+
+  function handleCheck(id) {
+    console.log("Checked");
+    const newTodos = [...todos];
+    console.log(newTodos);
+    const todo = newTodos[currentTodo].find((todo) => todo.key == id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  }
+
+  function handleAddTodo() {
+    if (text != "") {
+      const newTodos = [...todos];
+      newTodos[currentTodo] = [
+        ...newTodos[currentTodo],
+        { key: Date.now(), name: text, completed: false },
+      ];
+
+      setTodos(newTodos);
+      console.log(todos);
+      onChangeText("");
+    }
+  }
+
+  function handleRemoveCompletedTodos() {
+    console.log("Remove Todos");
+
+    const newTodos = [...todos];
+    newTodos[currentTodo] = todos[currentTodo].filter((todo) => {
+      return !todo.completed;
+    });
+    setTodos(newTodos);
+  }
+
+  function switchCurrentTodo() {
+    console.log("pressed");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* List */}
+      <ScrollView style={styles.todoList}>
+        <View style={{ height: "100%" }}>
+          {todos[currentTodo].map((todo) => (
+            <TodoItem
+              key={todo.key}
+              text={todo.name}
+              handleCheck={handleCheck}
+              id={todo.key}
+            />
+          ))}
+        </View>
+      </ScrollView>
+
+      {/* Footer input section */}
+      <View style={styles.footerContainer}>
+        <TextInput
+          style={styles.footerInput}
+          onChangeText={onChangeText}
+          placeholder="To do"
+          value={text}
+        />
+
+        {/* Add Todo Button */}
+        <View style={styles.footerButton}>
+          <TouchableOpacity onPress={handleAddTodo}>
+            <Icon
+              name="add"
+              style={styles.footerButtonIcon}
+              color={"#404040"}
+              reverse
+            />
+          </TouchableOpacity>
+        </View>
+
+        {/* Remove Completed Todos Button */}
+        <View style={(styles.footerButton, { marginRight: 0 })}>
+          <TouchableOpacity onPress={handleRemoveCompletedTodos}>
+            <Icon
+              name="delete"
+              style={styles.footerButtonIcon}
+              color={"#404040"}
+              reverse
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +113,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  todoList: {
+    marginTop: 50,
+    width: "95%",
+    height: 1000,
+    overflow: "visible",
+  },
+  // Footer styles
+  footerContainer: {
+    flexDirection: "row",
+    backgroundColor: "#e0e0e0",
+    width: "95%",
+    borderRadius: 10,
+    position: "absolute",
+    bottom: 10,
+  },
+  footerButton: {
+    justifyContent: "center",
+    alignItems: "stretch",
+    alignSelf: "stretch",
+    overflow: "hidden",
+    borderRadius: 50,
+    margin: -5,
+  },
+  footerButtonIcon: {
+    height: 30,
+    width: 30,
+    color: "#7d7d7d",
+  },
+  footerInput: {
+    flex: 1,
+    height: 50,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "#7d7d7d",
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 10,
   },
 });
---
+++ src/components/TodoItem.js
@@ -0,0 +1,53 @@
+import React, { useRef, useEffect } from "react";
+import {
+  Keyboard,
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+} from "react-native";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+
+export default function TodoItem({ text, completed, handleCheck, id }) {
+  const translation = useRef(
+    new Animated.Value(Dimensions.get("window").height - 200)
+  ).current;
+
+  useEffect(() => {
+    Animated.timing(translation, {
+      toValue: 0,
+      duration: 700,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
+  const styles = StyleSheet.create({
+    todoView: {
+      flexDirection: "row",
+      // backgroundColor: "#bdbdbd",
+      height: 50,
+      width: "100%",
+      marginBottom: 1,
+      paddingLeft: 10,
+      transform: [{ translateY: translation }],
+    },
+  });
+
+  return (
+    <Animated.View style={styles.todoView}>
+      <BouncyCheckbox
+        key={id}
+        onPress={(isChecked) => {
+          completed = isChecked;
+          handleCheck(id);
+        }}
+        fillColor="#bdbdbd"
+        unfillColor="#FFFFFF"
+        iconStyle={{ borderColor: "bdbdbd" }}
+        textStyle={{ color: "black" }}
+        text={text}
+      />
+    </Animated.View>
+  );
+}
